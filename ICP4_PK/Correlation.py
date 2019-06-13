723b0e2df75d7549736e33a917753dfd20b01535
pkankariya <pkkdg@mail.umkc.edu>
2019-06-13 14:37:33 -0500

    Correlation updated

--- ICP4_PK/Correlation.py
+++ ICP4_PK/Correlation.py
@@ -2,4 +2,25 @@
 import pandas as pd
 
 # Loading and reading the data set made avaialable
-glass_data = pd.read_csv('glass.csv')
+titanic_train = pd.read_csv('train_preprocessed.csv')
+titanic_test = pd.read_csv('test_preprocessed.csv')
+titanic = [titanic_train, titanic_test]
+
+print(titanic_train['Survived'].value_counts(dropna='False'))
+
+# Analyzing the data
+print('Training Data Set:')
+print(titanic_train.info())
+print('Test Data Set:')
+print(titanic_test.info())
+
+# Fixing null values
+print(titanic_train.isnull().sum())
+
+titanic_train["Embarked"] = titanic_train["Embarked"].fillna("S")
+#titanic_train.drop("Cabin", axis=1, inplace=True)
+
+# Analyze pivoting features
+result = titanic_train[['Sex', 'Survived']].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+print(result)
+
